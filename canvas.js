aa5a5eb8fe3375edcc2539af9f388032a04cd263
shukrullah shirzad <shukrullah.shirzad.779@gmail.com>
2022-02-26 09:26:05 +0430

    removed the blury effect from particles and added a new cancas element

--- canvas.js
+++ canvas.js
@@ -1,4 +1,4 @@
-const canvas = document.querySelector('#canvas');
+const canvas = document.querySelector('#canvas1');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -43,7 +43,7 @@ class Particle{
   update(){
     this.x += this.speedX;
     this.y += this.speedY;
-    if(this.size > 0.2) this.size -= 0.1;
+    // if(this.size > 0.2) this.size -= 0.1;
   }
   draw(){
     ctx.fillStyle = this.color; 
@@ -63,13 +63,12 @@ function handleParticles(){
       if(distance<100){
         ctx.beginPath();
         ctx.strokeStyle = particlesArray[i].color;
-        ctx.lineWidth = 0.2;
+        ctx.lineWidth = 0.4;
         ctx.moveTo(particlesArray[i].x, particlesArray[i].y);
         ctx.lineTo(particlesArray[j].x, particlesArray[j].y);
         ctx.stroke();
       }
     }
-    
     if(particlesArray[i].size <= 0.3){
       particlesArray.splice(i,1);
       i--;
@@ -78,7 +77,7 @@ function handleParticles(){
 }
 function animate(){
   //ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = 'rgba(0,0,0,0.02';
+  ctx.fillStyle = 'rgba(0,0,0,1)';
   ctx.fillRect(0, 0, canvas.width, canvas.height)
   handleParticles();
   hue += 5;
